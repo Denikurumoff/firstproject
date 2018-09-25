e7898646c95321637f90cd2fbb62dd9dd527ed5f
Denikurumoff <43386298+Denikurumoff@users.noreply.github.com>
2018-09-25 16:45:30 +0300

    Задание № 3 от Курумова

--- Input.java
+++ Input.java
@@ -9,10 +9,12 @@ public class Input {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("введите первое слагаемое");
+        System.out.println("введите первое число");
         float value = scanner.nextFloat();
-        System.out.println("введите второе слагаемое слагаемое");
+
+        System.out.println("введите второе число");
         float value_2 = scanner.nextFloat();
+
         System.out.printf("%.4f", value+value_2);
     }
 }
