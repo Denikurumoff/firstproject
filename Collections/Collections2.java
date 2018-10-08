abd2fc23886b289468f0907d9be0f2c08167c41f
Denikurumoff <43386298+Denikurumoff@users.noreply.github.com>
2018-10-08 10:48:16 +0300

    123

--- Collections/Collections2.java
+++ Collections/Collections2.java
@@ -14,16 +14,18 @@ package Collections;
 
 
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.*;
+import java.util.Collections;
 
 
-public class Collections {
+public class Collections2 {
     public static void main(String[] args) throws IOException {
 
         File file = new File("src\\main\\java\\Collections\\Collections.txt");
         int max;
         int min;
+        String emax ="";
+        String tmin = "";
 
 
         System.out.println("\n_______________________CПИСОК СЛОВ, ИМЕЮЩИХСЯ В ФАЙЛЕ________________________\n");
@@ -48,32 +50,45 @@ public class Collections {
         /*вывод списка в алфавитном порядке*/
         System.out.println("\n\n______________________ЧАСТОТА ПОВТОРЕНИЯ СЛОВ В МАССИВЕ________________________\n");
         for (int i = 0; i < array.size(); i++) {
-            System.out.println(array.get(i) + " Количество повторений: " + java.util.Collections.frequency(array, array.get(i)));
+            System.out.println(array.get(i) + " Количество повторений: " + Collections.frequency(array, array.get(i)));
 
         }
 
 
         System.out.println("\n______________СЛОВА С НАИБОЛЬШИМ И НАИМЕНЬШИМ КОЛ-ВОМ ПОВТОРЕНИЙ_______________\n");
-        for (int e = 0; e < (array.size() - 1); e++) {
-            if (java.util.Collections.frequency(array, array.get(e + 1)) > java.util.Collections.frequency(array, array.get(e))) {
-                max = java.util.Collections.frequency(array, array.get(e + 1));
-                System.out.println("элемент с максимальным числом повторений = " + array.get(e + 1) + " число повторений: " + max);
-                break;
+        max = 0;
+        for (int e = 0; e < array.size(); e++) {
+            if (Collections.frequency(array, array.get(e)) > max) {
+                max = Collections.frequency(array, array.get(e));
+                emax = array.get(e);
+
+
+
             }
-            for (int t = 0; t < (array.size() - 1); t++) {
-                if (java.util.Collections.frequency(array, array.get(t + 1)) < java.util.Collections.frequency(array, array.get(t))) {
-                    min = java.util.Collections.frequency(array, array.get(t + 1));
-                    System.out.println("элемент с минимальным числом повторений = " + array.get(t + 1) + " число повторений: " + min);
-                    break;
-                }
+
+
+        }
+        System.out.println("элемент с максимальным числом повторений = " + emax + " число повторений: " + max);
+
+        min = 1;
+        for (int t = 0; t < array.size(); t++) {
+            if (Collections.frequency(array, array.get(t)) <= min) {
+                min = Collections.frequency(array, array.get(t));
+                tmin = array.get(t);
+
+
             }
+
 
         }
 
+        System.out.println("элемент с минимальным числом повторений = " + tmin + " число повторений: " + min);
         System.out.println("\n\n_______________________________________________________________________________\n");
-
     }
+
+
 }
+
 
 
 
